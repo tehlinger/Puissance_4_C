2b5da69a9b705351880a2c77588f661abd9bbf89
Thibaut Ehlinger <thibaut.ehlinger@free.fr>
2012-11-18 10:10:32 +0100

    Corection du "bug de la diagonale positive"

--- src/verifications.c
+++ src/verifications.c
@@ -1,12 +1,7 @@
 #include <verifications.h>
 
 int contenu_case(int x, int y, partie p){
-	 if((((p->grille) [x-1]) -> h) < y) return -1;//Si la hauteur de la pile n'atteind pas encore la case, renvoie -1
-	 else
-	 {
-		 if ((((p->grille)[x-1])->valeurs[y-1]) == 0) return 0;
-		 else return 1;
-	 }
+return ((p->grille[x-1])->valeurs[y-1]);
 	 }
 
 int dernier_coup (partie p){
@@ -65,7 +60,7 @@ int alignes_diag_gauche (bool joueur, int indice, partie p) {
 	hauteur_partie_gauche--;
 }
 	
-	return (acc_gauche + acc_droit + 1);
+	return (acc_gauche + acc_droit +1);
 	
 }
 
@@ -75,20 +70,20 @@ int alignes_diag_droite(bool joueur, int indice, partie p){
 	int hauteur_coup = ((((p->grille)[indice-1])->h) + 1); //Hauteur du coup en question
 	int acc_gauche = 0;
 	int acc_droit = 0;
-	int hauteur_partie_droite = hauteur_coup - 1;
-	int hauteur_partie_gauche = hauteur_coup + 1;
+	int hauteur_partie_droite = hauteur_coup + 1;
+	int hauteur_partie_gauche = hauteur_coup - 1;
 	
-	for(i = indice - 1; (i > 0) && (contenu_case(i, hauteur_partie_droite, p) == joueur) && (hauteur_partie_droite < HAUTEUR); i--){
-	hauteur_partie_droite --;
+	for(i = indice - 1; (i > 0) && (contenu_case(i, hauteur_partie_gauche, p) == joueur) && (hauteur_partie_gauche > 0); i--){
+	hauteur_partie_gauche --;
 	acc_gauche ++;
 	}
 	
-	for ((i=indice + 1); (i <= 7) && (contenu_case(i, hauteur_partie_gauche, p) == joueur) && (hauteur_partie_gauche > 0); i++){
+	for ((i=indice + 1); (i <= 7) && (contenu_case(i, hauteur_partie_droite, p) == joueur) && (hauteur_partie_droite < HAUTEUR); i++){
 	acc_droit ++;
-	hauteur_partie_gauche++;
+	hauteur_partie_droite ++;
 }
 	
-	return (acc_gauche + acc_droit + 1);
+	return (acc_gauche + acc_droit +1);
 	
 }
 
